fc837f259184ea77d00b383058c1e1fdd7f944e0
Weverttonbruno <evoce773@gmail.com>
2021-08-23 18:22:25 -0300

    python

---
+++ helloword.py
@@ -0,0 +1,13 @@
+a = 18
+b = 8 
+soma = a + b
+subtracao = a - b
+mutiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('soma: {}' .format(soma))
+print('subtracao: {}' .format(subtracao))
+print('multiplicacao: {}' .format(mutiplicacao))
+print('divisao: {}' .format(divisao))
+
+print('resto:{}' .format(resto))
