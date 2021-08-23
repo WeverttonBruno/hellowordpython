8402c5e06ec449a75f6d82d9436179418f4208c2
Weverttonbruno <evoce773@gmail.com>
2021-08-23 19:48:46 -0300

    python

--- helloword.py
+++ helloword.py
@@ -1,5 +1,8 @@
-a = 18
-b = 8 
+#Todos calculos fácil
+print('TELA DE CONTAGEM')
+a = int(input ('Digite qualquer valor:'))
+b = int(input ('Digite qualquer valor:'))
+print(type(a))
 soma = a + b
 subtracao = a - b
 mutiplicacao = a * b
@@ -10,4 +13,23 @@ print('subtracao: {}' .format(subtracao))
 print('multiplicacao: {}' .format(mutiplicacao))
 print('divisao: {}' .format(divisao))
 
+#calculo maior que
+
 print('resto:{}' .format(resto))
+print('TELA DE MAIOR NÚMERO')
+if a > b:
+        print('o maior número é {}' .format(a))
+
+else:
+        print('o maior número é {}' .format(b))
+
+#Para mostrar se é par ou impar
+a = int(input('Entre com um valor'))
+resto = a % 2
+if resto == 0:
+    print('Número é par')
+else:
+    print('Número é impar')
+
+
+print('FINAL DO PROGRAMA')
